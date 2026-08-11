367645cd887eb887de3a1614ce0b3d7d8ff9e662
emma <formula865@yahoo.com>
2026-08-11 11:55:57 +0000

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Employee } from './elevat/employee/employee';
+import { Leave } from './elevat/leave/leave';
+import { Pagelayout } from './elevat/pagelayout/pagelayout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    {path: "", component: Pagelayout,
+        children:[
+    {path: "", redirectTo: "employee", pathMatch: "full"},
+    {path: "employee", component: Employee},
+    {path: "leave", component: Leave},
+
+        ]
+    }
+];
---
+++ src/app/elevat/pagelayout/pagelayout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router'; 
+
+@Component({
+  selector: 'app-pagelayout',
+  imports: [RouterOutlet],
+  templateUrl: './pagelayout.html',
+  styleUrl: './pagelayout.scss',
+})
+export class Pagelayout {
+
+}
