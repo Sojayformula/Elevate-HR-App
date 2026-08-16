c81f56862781cf7a2012b5beb2ad2ec9fcbaab2f
Emmanuel Sowah <Emmanuel.Sowah@ETRANZACTGH.COM>
2026-08-16 19:23:27 +0000

    updated

--- src/app/elevate/elevate/elevate.ts
+++ src/app/elevate/elevate/elevate.ts
@@ -14,7 +14,7 @@ export class Elevate {
 
      activeTab = 'target';
       isActiveTab = 'target';
-      currentStep: number = 1;
+      currentStep = 1;
       totalStep = 4
       message = ""
       coments = ""
@@ -41,7 +41,6 @@ export class Elevate {
 
 
 tabActive(tab: string) {
-   console.log('CLICKED:', tab);
   this.isActiveTab = tab;
 
   switch (tab) {
@@ -61,7 +60,7 @@ tabActive(tab: string) {
       this.currentStep = 4;
       break;
   }
-  console.log('ACTIVE:', this.isActiveTab);
+  
 }
 
 
