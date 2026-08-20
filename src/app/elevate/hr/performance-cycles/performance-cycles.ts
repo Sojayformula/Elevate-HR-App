aa8151f1e985e470fb3c0875d738fb1738f8927c
eyiram-agborson <eyiram.agborson@etranzact.com.gh>
2026-08-20 15:05:55 +0000

    HR components generated

---
+++ src/app/elevate/hr/performance-cycles/performance-cycles.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-performance-cycles',
+  imports: [],
+  templateUrl: './performance-cycles.html',
+  styleUrl: './performance-cycles.scss',
+})
+export class PerformanceCycles {
+
+}
---
+++ src/app/elevate/hr/set-kpi/set-kpi.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-set-kpi',
+  imports: [],
+  templateUrl: './set-kpi.html',
+  styleUrl: './set-kpi.scss',
+})
+export class SetKpi {
+
+}
