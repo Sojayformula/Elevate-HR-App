af80f58649df52db0f0c01cb396ab4766b35a744
eyiram-agborson <eyiram.agborson@etranzact.com.gh>
2026-08-20 19:23:57 +0000

    Performance routes done

--- src/app/elevate/hr/performance-cycles/edit-cycle/edit-cycle.ts
+++ src/app/elevate/hr/performance-cycles/edit-cycle/edit-cycle.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-cycle',
-  imports: [],
+  imports: [ReactiveFormsModule ],
   templateUrl: './edit-cycle.html',
   styleUrl: './edit-cycle.scss',
 })
 export class EditCycle {
+  cycleForm = new FormGroup({
+  cycleName: new FormControl('', Validators.required),
+  cycleStartDate: new FormControl('', Validators.required),
+  cycleEndDate: new FormControl('', Validators.required),
+  
+  targetStartDate: new FormControl('', Validators.required),
+  targetEndDate: new FormControl('', Validators.required),
 
+  reviewStartDate: new FormControl('', Validators.required),
+  reviewEndDate: new FormControl('', Validators.required),
+  activateCycle: new FormControl(true),
+  })
+
+  updateCycle(){
+    console.log(this.cycleForm.value)
+  }
 }
