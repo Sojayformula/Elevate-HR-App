fcd220e80b1ba6795a7ed9df36be3961e6b8814e
eyiram-agborson <eyiram.agborson@etranzact.com.gh>
2026-08-21 12:11:39 +0000

    Create-kpi basic template done

--- src/app/elevate/hr/set-kpi/create-kpi/create-kpi.ts
+++ src/app/elevate/hr/set-kpi/create-kpi/create-kpi.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-create-kpi',
-  imports: [],
+  imports: [NzDropDownModule, NzIconModule ],
   templateUrl: './create-kpi.html',
   styleUrl: './create-kpi.scss',
 })
 export class CreateKpi {
-
+kpiForm = new FormGroup({
+  department: new FormControl('', Validators.required),
+  jobTitle: new FormControl('', Validators.required)
+});
 }
